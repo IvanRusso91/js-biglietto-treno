3ebd55ce14a30f00530246b444b84860c9f7b562
IvanRusso91 <iv.russo91@gmail.com>
2022-04-06 15:07:16 +0200

    inserito punto di partenza

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+/*
+1. Km da percorrere.
+2. chiedere all'utente età.
+3. porre l'eventuale sconto 40% per gli  over 65 anni o il 20% per gli under 18.
+4.calcolare il biglietto.
+5.stampare a schermo.
+*/
